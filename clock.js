bcdf11d8b032a8a7d66359f98eedcfea3435a4a4
Andy Jiang <andyjiang@gmail.com>
2014-11-02 14:15:41 -0800

    derp

---
+++ clock.js
@@ -0,0 +1,10 @@
+
+var CronJob = require('cron').CronJob;
+var test = require('./bot.js').test;
+
+new CronJob({
+  cronTime: "15 * * * * *",//15 seconds after every minute
+  onTick: test,
+  start: true,
+  timeZone: "America/Los_Angeles"
+});
