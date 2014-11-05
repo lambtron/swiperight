7b2b5864f773da685f833636a3668b06e8af5b16
Andy Jiang <andyjiang@gmail.com>
2014-11-04 19:16:13 -0800

    no need to explicitly set auth token

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 // top level libraries
-var authToken = '89142d91-269d-44aa-8b52-7e259a3a713a';
+var fbId = '1227960057';
+var fbToken = 'CAAGm0PX4ZCpsBAHQg0mZCYRyteKlsHV06kiHqDgXu0ZApMZCjB3eijhixOOY5owYo586sPnpDqyVejzDMBIapZBCiZCiHgTgKV7qsLF0utnePOasnBaRj7bZAiRTejOLNx28RmmYTQpn7ZAy2BahtxJRi5S6GWXVXOIIlP7APIe0VatC5GrZCMmUzjX9j3QytZAvjFaZBrOnLpEpsVr65K4rfdqUEYZAIJnZBsxx60RINKGsHIAZDZD';
 var Tinder = require('./lib/tinderjs/tinder.js').TinderClient;
 var tinder = new Tinder();
 
@@ -12,10 +13,6 @@ var LIKEINTERVAL = 4;
 var LIKEINTERVAL_STD = 0.7;
 
 
-// configuration.
-tinder.setAuthToken(authToken);
-
-
 /**
  * starts the liking bonanza
  *
@@ -23,37 +20,40 @@ tinder.setAuthToken(authToken);
  * @param {Function} cb
  */
 function start(sessionLength, cb) {
-  var likeRecs = function likeRecs(err, recs) {
-    if (err)
-      cb(err, null);
+  tinder.authorize(fbToken, fbId, function() {
+    // Sets the token.
+    var likeRecs = function likeRecs(err, recs) {
+      if (err)
+        cb(err, null);
 
-    if (recs && recs.results) {
-      (function like(i) {
-        var newInterval = ((Math.random() * LIKEINTERVAL_STD * 2 - (LIKEINTERVAL_STD / 2)) + LIKEINTERVAL) * 100;
-        setTimeout(function() {
-          var id = recs.results[i - 1]._id;
-          console.log('swiping right on ' + recs.results[i - 1].name);
-          tinder.like(id, function(err, data) {
-            console.log(data);
-          });
-          if (--i) like(i);      //  decrement i and call myLoop again if i > 0
-        }, newInterval);
-      })(recs.results.length);
-    } else {
-      cb(err, null);
-    }
-  };
+      if (recs && recs.results) {
+        (function like(i) {
+          var newInterval = ((Math.random() * LIKEINTERVAL_STD * 2 - (LIKEINTERVAL_STD / 2)) + LIKEINTERVAL) * 100;
+          setTimeout(function() {
+            var id = recs.results[i - 1]._id;
+            console.log('swiping right on ' + recs.results[i - 1].name);
+            tinder.like(id, function(err, data) {
+              console.log(data);
+            });
+            if (--i) like(i);      //  decrement i and call myLoop again if i > 0
+          }, newInterval);
+        })(recs.results.length);
+      } else {
+        cb(err, null);
+      }
+    };
 
-  tinder.getRecommendations(10, likeRecs);
-
-  // session variables.
-  var startTime = Date.now();
-  var timer = setInterval(function() {
-    var now = Date.now();
-    if (now > startTime + (sessionLength * 100))
-      clearInterval(timer);
     tinder.getRecommendations(10, likeRecs);
-  }, GETRECSINTERVAL_MEAN * 100);
+
+    // session variables.
+    var startTime = Date.now();
+    var timer = setInterval(function() {
+      var now = Date.now();
+      if (now > startTime + (sessionLength * 100))
+        clearInterval(timer);
+      tinder.getRecommendations(10, likeRecs);
+    }, GETRECSINTERVAL_MEAN * 100);
+  });
 };
 
 
@@ -61,3 +61,5 @@ function start(sessionLength, cb) {
 module.exports = {
   start: start
 };
+
+start(10, function() {});
